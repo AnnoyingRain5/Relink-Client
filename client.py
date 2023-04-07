c6a3cfe65228205be32ff1d93a40ce001adc4a6c
AnnoyingRains <AnnoyingRain5@gmail.com>
2023-04-07 23:32:44 +1000

    Client now has a TUI

--- client.py
+++ client.py
@@ -3,16 +3,53 @@ import asyncio
 import aioconsole
 import communication
 import json
+import os
+import math
+import sys
 
+CURSOR_HOME = '\033[H'
 CURSOR_UP = '\033[F'
 ERASE_LINE = '\033[K'
+ERASE_SCREEN = '\033[2J'
 
+BLUE_BACKGROUND = '\033[48;5;57m'
 YELLOW = '\033[33m'
 RED = '\033[31m'
 BLUE = '\033[94m'
 NORMAL = '\033[0m'
 
 username = ""
+messages = []
+channel = "Unknown (not supplied by server?)"
+serverAddress = ""
+
+
+def renderText():
+    lines = os.get_terminal_size().lines
+    columns = os.get_terminal_size().columns
+    topstr = f"#{channel}@{serverAddress}"
+    print(f"{ERASE_SCREEN}{CURSOR_HOME}{BLUE_BACKGROUND}{topstr}", end="")
+    for column in range(columns - len(topstr)):
+        print(" ", end="")
+    print(NORMAL)
+    for message in messages:
+        if "\n" in message:
+            lines -= (message.count("\n"))
+            continue
+        plain = message.replace(YELLOW, "").replace(
+            RED, "").replace(BLUE, "").replace(NORMAL, "")
+        lines -= math.ceil(len(plain) / columns) - 1
+    while len(messages) > lines - 2:
+        message = messages.pop(0)
+        lines += (message.count("\n"))
+        plain = message.replace(YELLOW, "").replace(
+            RED, "").replace(BLUE, "").replace(NORMAL, "")
+        lines += math.ceil(len(plain) / columns) - 1
+    for line in range(lines - 2):
+        try:
+            print(messages[line])
+        except:
+            print("")
 
 
 async def PacketReciever(websocket):
@@ -25,27 +62,36 @@ async def PacketReciever(websocket):
                 await commandHandler(websocket, packet)  # type: ignore
             case communication.system:
                 await SystemMessageHandler(websocket, packet)  # type: ignore
+            case communication.channelChange:
+                await ChannelChangeHandler(websocket, packet)  # type: ignore
             case _:
                 print(type(packet))
+                exit()
 
 
 async def messageHandler(websocket, message: communication.message):
     if message.username == username:
-        print(f"{CURSOR_UP}{BLUE}{message.username}{NORMAL}: {message.text}")
+        messages.append(f"{BLUE}{message.username}{NORMAL}: {message.text}")
     else:
-        print(f"{RED}{message.username}{NORMAL}: {message.text}")
+        messages.append(f"{RED}{message.username}{NORMAL}: {message.text}")
+    renderText()
 
 
 async def SystemMessageHandler(websocket, message: communication.system):
-    finalmessage = ""
-    if message.response:
-        finalmessage += CURSOR_UP
-    finalmessage += f"{YELLOW}{message.text}{NORMAL}"
-    print(finalmessage)
+    messages.append(f"{YELLOW}{message.text}{NORMAL}")
+    renderText()
 
 
 async def commandHandler(websocket, command: communication.command):
     pass
+
+
+async def ChannelChangeHandler(websocket, message: communication.channelChange):
+    global channel
+    global messages
+    channel = message.channel
+    messages = []
+    renderText()
 
 
 async def inputmanager(websocket):
@@ -71,12 +117,13 @@ async def inputmanager(websocket):
 
 async def main():
     # connect and log in or sign up to the server
-    server_address = input(
+    global serverAddress
+    serverAddress = input(
         "Please enter the IP address or domain name (excluding port) of the server you wish to connect to: ")
     print("Please wait while we attempt to connect to the server...")
     try:
         async with websockets.connect(  # type: ignore
-                f"ws://{server_address}:8765") as websocket:
+                f"ws://{serverAddress}:8765") as websocket:
             global username
             action = input(
                 "Log in or sign up? Type l for login and s for sign up: ").lower()
@@ -111,6 +158,7 @@ async def main():
                     response.json = await websocket.recv()
                     if response.result == True:
                         print("Login successful!")
+                        renderText()
                         break
                     else:
                         print("Denied! Please try again...")
@@ -122,7 +170,7 @@ async def main():
             # both of these loop forever, so technically we do not need to wait for both of them
             await PktRcvTask
             await InputManTask
-    except websockets.exceptions.ConnectionClosedOK:
+    except websockets.exceptions.ConnectionClosedOK:  # type: ignore
         print(f"{YELLOW}The connection has been closed by the server.{NORMAL}")
 
 asyncio.run(main())
