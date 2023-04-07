45e1edf1605b895e2316b371fddb0c7a23190d0e
BuildTools <unconfigured@null.spigotmc.org>
2023-04-07 12:21:28 +1000

    Display a message when the server exits

--- client.py
+++ client.py
@@ -74,53 +74,55 @@ async def main():
     server_address = input(
         "Please enter the IP address or domain name (excluding port) of the server you wish to connect to: ")
     print("Please wait while we attempt to connect to the server...")
+    try:
+        async with websockets.connect(  # type: ignore
+                f"ws://{server_address}:8765") as websocket:
+            global username
+            action = input(
+                "Log in or sign up? Type l for login and s for sign up: ").lower()
+            if action == "s":  # sign up
+                while True:
+                    username = await aioconsole.ainput("What username will you go by? ")
+                    password = await aioconsole.ainput(
+                        "Make an unforgettable password: ")
+                    packet = communication.signupRequest()
+                    packet.username = username
+                    packet.password = password
+                    print("Please wait...")
+                    await websocket.send(packet.json)
+                    response = communication.result()
+                    response.json = await websocket.recv()
+                    if response.result == True:
+                        print("Login successful!")
+                        break
+                    else:
+                        print("Denied! Please try again...")
+                        print(f"The server said: {response.reason}")
+            elif action == "l":  # log in
+                while True:
+                    username = await aioconsole.ainput("What is your username? ")
+                    password = await aioconsole.ainput("What is your password? ")
+                    packet = communication.loginRequest()
+                    packet.username = username
+                    packet.password = password
+                    print("Please wait...")
+                    await websocket.send(packet.json)
+                    response = communication.result()
+                    response.json = await websocket.recv()
+                    if response.result == True:
+                        print("Login successful!")
+                        break
+                    else:
+                        print("Denied! Please try again...")
+                        print(f"The server said: {response.reason}")
 
-    async with websockets.connect(  # type: ignore
-            f"ws://{server_address}:8765") as websocket:
-        global username
-        action = input(
-            "Log in or sign up? Type l for login and s for sign up: ").lower()
-        if action == "s":  # sign up
-            while True:
-                username = await aioconsole.ainput("What username will you go by? ")
-                password = await aioconsole.ainput(
-                    "Make an unforgettable password: ")
-                packet = communication.signupRequest()
-                packet.username = username
-                packet.password = password
-                print("Please wait...")
-                await websocket.send(packet.json)
-                response = communication.result()
-                response.json = await websocket.recv()
-                if response.result == True:
-                    print("Login successful!")
-                    break
-                else:
-                    print("Denied! Please try again...")
-                    print(f"The server said: {response.reason}")
-        elif action == "l":  # log in
-            while True:
-                username = await aioconsole.ainput("What is your username? ")
-                password = await aioconsole.ainput("What is your password? ")
-                packet = communication.loginRequest()
-                packet.username = username
-                packet.password = password
-                print("Please wait...")
-                await websocket.send(packet.json)
-                response = communication.result()
-                response.json = await websocket.recv()
-                if response.result == True:
-                    print("Login successful!")
-                    break
-                else:
-                    print("Denied! Please try again...")
-                    print(f"The server said: {response.reason}")
-
-        # we are now fully logged into the server, start the main script
-        PktRcvTask = asyncio.create_task(PacketReciever(websocket))
-        InputManTask = asyncio.create_task(inputmanager(websocket))
-        # both of these loop forever, so technically we do not need to wait for both of them
-        await PktRcvTask
-        await InputManTask
+            # we are now fully logged into the server, start the main script
+            PktRcvTask = asyncio.create_task(PacketReciever(websocket))
+            InputManTask = asyncio.create_task(inputmanager(websocket))
+            # both of these loop forever, so technically we do not need to wait for both of them
+            await PktRcvTask
+            await InputManTask
+    except websockets.exceptions.ConnectionClosedOK:
+        print(f"{YELLOW}The connection has been closed by the server.{NORMAL}")
 
 asyncio.run(main())
